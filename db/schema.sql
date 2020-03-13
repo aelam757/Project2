6be64605a5ff21d184557702070dde0d7dfd64c4
Micah Evans <micah.s.evans@gmail.com>
2020-03-12 20:32:03 -0400

    Adding rest of files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DELETE TABLE IF EXISTS interview_ratingsDB;
+CREATE TABLE interview_ratingsDB;
+
+company_id AUTO INCREMENT NOT NULL,
+Company_name
+Company_industry (5 main tech industries)
+Company_benefits (dental, vision, health, etc.)
+Company_url
+
+Company_rating 5-star rating
