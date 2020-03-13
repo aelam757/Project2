6e9c10116ff9cb2633ab0d5860e6e3ca4908f937
Micah Evans <micah.s.evans@gmail.com>
2020-03-12 20:37:51 -0400

    Fleshed out Schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
 DELETE TABLE IF EXISTS interview_ratingsDB;
 CREATE TABLE interview_ratingsDB;
 
-company_id AUTO INCREMENT NOT NULL,
-Company_name
-Company_industry (5 main tech industries)
-Company_benefits (dental, vision, health, etc.)
-Company_url
+USE interview_ratingsDB;
 
-Company_rating 5-star rating
+company_id AUTO INCREMENT NOT NULL,
+PRIMARY KEY (company_id),
+Company_name VARCHAR(200) NOT NULL,
+Company_industry VARCHAR(200),
+Company_benefits VARCHAR(500),
+Company_url VARCHAR(500),
+Company_rating DECIMAL(10,1)
