8c6515a27ae715c32b89d6d71a8a33af770cca97
Alan Elam <aelam757@gmail.com>
2020-03-18 18:55:04 -0400

    software enginner info added to 15 companies for testing js

--- db/seed.sql
+++ db/seed.sql
@@ -1,21 +1,40 @@
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Microsoft');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Charter Global Inc.');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Delta');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('UPS');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('The Home Depot');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Booz Allen Hamilton');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('SunTrust');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Deloitte');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Anthem Inc.');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Comcast');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('The Coca-Cola Company');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Cox Enterprises');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('PGi');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Cox Media Group');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Southern Company');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('First Data');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('NCR');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('WayStar');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Emory Healthcare');
-INSERT INTO companies (company_name,rating,benefits,company_url,position,salary) VALUES ('Citi');
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Microsoft', 4, 4, 5,'Software Engineer', '$54k-$123k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Charter Global Inc.', 3.5, 3.5, 5, 'Software Engineer', '$80k-$102k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Delta', 4, 4, 5,'Software Engineer', '$67k-$73k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('UPS', 4, 3.5, 5,'Software Engineer', '$67k-$73k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('The Home Depot', 4, 3.5, 5, 'Software Engineer', '$62k-$144k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Booz Allen Hamilton', 4, 4, 5, 'Software Engineer', '$87k-$95k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('SunTrust', 3.5, 4, 5, 'Software Engineer', '$41k-$115k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Deloitte', 4, 4, 5, 'Software Engineer', '$65k-$182k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Anthem Inc.', 3.5, 3, 5, 'Software Engineer', '$70-$133k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Comcast', 3.5, 3.5 , 5, 'Software Engineer', '$63k-$174k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('The Coca-Cola Company', 4, 4, 5, 'Software Engineer', '$97k-$104k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Cox Enterprises', 4, 4, 5, 'Software Engineer', '$98k-$116k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('PGi', 3.5, 3, 5, 'Software Engineer', '$93k-$143k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Cox Media Group', 4, 3.5, 5, 'Software Engineer', '$91k-$126k');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Southern Company');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('First Data');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('NCR');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('WayStar');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Emory Healthcare');
+
+INSERT INTO companies (company_name,Irating,Grating,Drating,position,salary) VALUES ('Citi');
 
