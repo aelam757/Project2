3b77e62eb94d79e4122a43a4338c1a2222697019
Jack Markwalter <jackmarkwalter3@gmail.com>
2020-03-18 19:13:30 -0400

    server, but not completed

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DELETE DATABASE IF EXISTS maji_db;
+
+CREATE DATABASE maji_db;
+
+USE maji_db;
+
+CREATE TABLE companies
+(
+company_id AUTO INCREMENT NOT NULL,
+Company_name VARCHAR(200) NOT NULL,
+Company_industry VARCHAR(200),
+Company_benefits VARCHAR(500),
+Company_url VARCHAR(500),
+Company_rating DECIMAL(10,1),
+Company_position VARCHAR(500),
+Company_salary INT,
+PRIMARY KEY (company_id)
+);
+
+CREATE TABLE clients
+(
+	user_id INT NOT NULL AUTO INCREMENT,
+    client_name VARCHAR(200) NOT NULL,
+    user_rating DECIMAL(10,1),
+    salary_rating DECIMAL(10,1),
+    benefits_rating DECIMAL(10,1),
+    interview_rating DECIMAL(10,1),
+	PRIMARY KEY(user_id)
+);
+
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,53 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-let isAuthenticated = require("../config/middleware/isAuthenticated");
+//Routes
 
 module.exports = function(app) {
 
+  //Each of the below routes handles the HTML page that the user gets sent to.
+  
+  //Index route loads view.html
+
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/homepage.html"));
+
+  app.get("/joblisting", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/joblisting.html"))
   });
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.get("/companypage", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/companyPage.html"))
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
 
-};
+// Passport Information Kept Below in Comments
+
+// // Requiring our custom middleware for checking if a user is logged in
+// let isAuthenticated = require("../config/middleware/isAuthenticated");
+
+// module.exports = function(app) {
+
+//   app.get("/", (req, res) => {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/signup.html"));
+//   });
+
+//   app.get("/login", (req, res) => {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+//   });
+
+//   // Here we've add our isAuthenticated middleware to this route.
+//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
+//   app.get("/members", isAuthenticated, (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/members.html"));
+//   });
+
+// };
